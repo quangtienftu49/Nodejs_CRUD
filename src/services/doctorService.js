551753b23b03ad3894388e3f4b9d2aec46b7687a
quangtienftu49 <50720010+quangtienftu49@users.noreply.github.com>
2022-02-05 18:58:32 -0500

    change key to keyMap in db to use belongsto and hasmany

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -8,9 +8,22 @@ let getTopDoctorHome = (limitInput) => {
         where: { roleId: "R2" },
         order: [["createdAt", "DESC"]],
         attributes: {
-          exclude: ["password", "image"],
+          exclude: ["password"],
         },
-        // raw: true,
+        include: [
+          {
+            model: db.Allcode,
+            as: "positionData",
+            attributes: ["valueEn", "valueVi"],
+          },
+          {
+            model: db.Allcode,
+            as: "genderData",
+            attributes: ["valueEn", "valueVi"],
+          },
+        ],
+        raw: true,
+        nest: true,
       });
 
       resolve({
