ff147b2f1a81e1862d0eb9977c644395f48cc81e
quangtienftu49 <50720010+quangtienftu49@users.noreply.github.com>
2022-02-05 22:18:02 -0500

    confi markdown model

---
+++ src/migrations/migration-markdown.js
@@ -0,0 +1,40 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("markdowns", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      contentHTML: {
+        allowNull: false,
+        type: Sequelize.TEXT("long"),
+      },
+      contentMarkdown: {
+        allowNull: false,
+        type: Sequelize.TEXT("long"),
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.TEXT("long"),
+      },
+      doctorId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      specialtyId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      clinicId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("markdowns");
+  },
+};
