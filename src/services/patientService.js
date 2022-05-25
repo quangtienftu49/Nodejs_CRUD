28279ffc6892a01f961915dac3aece69b474566f
quangtienftu49 <50720010+quangtienftu49@users.noreply.github.com>
2022-05-25 00:17:49 -0400

    use findOrCreate to upsert patient booking

--- src/services/patientService.js
+++ src/services/patientService.js
@@ -4,13 +4,15 @@ require("dotenv").config();
 let postBookAppointment = (data) => {
   return new Promise(async (resolve, reject) => {
     try {
-      if (!data.email) {
+      if (!data.email || !data.doctorId || !data.timeType || !data.date) {
         resolve({
           errCode: 1,
           errMessage: "Missing required parameters!",
         });
       } else {
         //upsert patient
+        // findOrCreate is to check if record is existed >> not recorded,
+        // if not existed >> inserted
         let user = await db.User.findOrCreate({
           where: { email: data.email },
           defaults: {
@@ -22,10 +24,22 @@ let postBookAppointment = (data) => {
         console.log("check user", user[0]);
 
         //create a booking record
+        if (user && user[0]) {
+          await db.Booking.findOrCreate({
+            where: { patientId: user[0].id },
+            defaults: {
+              statusId: "S1",
+              doctorId: data.doctorId,
+              patientId: user[0].id,
+              date: data.date,
+              timeType: data.timeType,
+            },
+          });
+        }
 
         resolve({
           errCode: 0,
-          errMessage: "Save doctor infor successfully!",
+          errMessage: "Save patient infor successfully!",
         });
       }
     } catch (e) {
