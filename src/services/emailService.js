08b8e1d9647465b2a94ac9bed8874b6bb994573b
quangtienftu49 <50720010+quangtienftu49@users.noreply.github.com>
2022-06-19 23:18:40 -0400

    build send attachment service

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -71,6 +71,61 @@ let getBodyHTMLEmail = (dataSent) => {
   return result;
 };
 
+let getBodyHTMLEmailPrescription = (dataSent) => {
+  let result = "";
+
+  if (dataSent.language === "vi") {
+    result = `
+    <h3>Xin chào ...!</h3>
+    <p>Bạn nhận được email này vì đã đặt lịch khám bệnh trên BookingDoctors.</p>
+    <p>Thông tin đơn thuốc và hóa đơn được gửi trong file đính kèm.</p>
+    
+    
+    
+    <div>Xin chân thành cảm ơn!</div>
+    `;
+  } else if (dataSent.language === "en") {
+    result = `
+    <h3>Dear ...,</h3>
+    <p>This email was sent to you because you booked an appointment on BookingDoctors.</p>
+    <p>The receipt and prescription as attachments.</p>
+    
+    <div>Thank you for booking with us!</div>
+    `;
+  }
+
+  return result;
+};
+
+let sendAttachment = async (dataSent) => {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_APP, // generated ethereal user
+      pass: process.env.EMAIL_APP_PASSWORD, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"TIEN DINH 👻" <quangtienftu49@gmail.com>', // sender address
+    to: dataSent.receiverEmail, // list of receivers
+    subject: "Kết quả đặt lịch khám bệnh", // Subject line
+    html: getBodyHTMLEmailPrescription(dataSent),
+    attachments: [
+      {
+        // encoded string as an attachment
+        filename: "text1.png",
+        content: "aGVsbG8gd29ybGQh",
+        encoding: dataSent.imgBase64,
+      },
+    ],
+  });
+};
+
 module.exports = {
   sendSimpleEmail: sendSimpleEmail,
+  sendAttachment: sendAttachment,
 };
